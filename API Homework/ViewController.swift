248708854c6b0e3174f6b096f18090d8a987394e
Nguyen Khac Thai <zed999cz@gmail.com>
2023-07-11 10:28:07 +0700

    Initial Commit

---
+++ API Homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  API Homework
+//
+//  Created by Thai Nguyen Khac on 11/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
