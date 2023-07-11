2dabd1a574b4cf55ae51359906be4a3dcbf24401
Nguyen Khac Thai <zed999cz@gmail.com>
2023-07-11 10:28:07 +0700

    [Task] Create UI and API Caller

---
+++ API Homework/Constants.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+struct Constants{
+    static let baseURL            = "https://api.github.com"
+    static let toUserProfileSegue = "toUserProfileSegue"
+}
---
+++ API Homework/ServiceManager.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+final class ServiceManager {
+    static let shared = ServiceManager()
+    
+    private init() {}
+    
+    private let API = APICaller.shared
+    
+    func getSearchUser(request params: SearchUserRequestParams, completion: @escaping(Result<APISearchResponse, Error>) -> Void) {
+        return API.getRequest(request: params, baseURL: Constants.baseURL, endpoint: EndPoints.searchEndPoint.rawValue, completion: completion)
+    }
+    
+    func getProfileUser(request params: ProfileUserRequestParams, completion: @escaping(Result<APIProfileResponse, Error>) -> Void) {
+        return API.getRequest(request: params, baseURL: Constants.baseURL, endpoint: EndPoints.profileEndPoint.rawValue, completion: completion)
+    }
+    
+    func getFollowersUser(request params: FollowersUserRequestParams, completion: @escaping(Result<APIFollowerResponse, Error>) -> Void) {
+        return API.getRequest(request: params, baseURL: Constants.baseURL, endpoint: EndPoints.profileEndPoint.rawValue, completion: completion)
+    }
+    
+    func getFollowingsUser(request params: FollowingUserRequestParams, completion: @escaping(Result<APIFollowerResponse, Error>) -> Void) {
+        return API.getRequest(request: params, baseURL: Constants.baseURL, endpoint: EndPoints.profileEndPoint.rawValue, completion: completion)
+    }
+}
